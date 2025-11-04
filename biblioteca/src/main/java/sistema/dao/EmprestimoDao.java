1dbe781651630c2de129255ab9c878455bcbe06e
Lucas Brembati Weizenmann <lucas8bitbw@gmail.com>
2025-11-04 19:03:42 -0400

    exclusão da classe SistemaDao

--- biblioteca/src/main/java/sistema/dao/EmprestimoDao.java
+++ biblioteca/src/main/java/sistema/dao/EmprestimoDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import sistema.Conexao;
@@ -128,5 +129,38 @@ public class EmprestimoDao {
 
         stmt.execute();
         stmt.close();
-    }  
+    }
+
+    public void registrarEmprestimo(Emprestimo emprestimo) throws SQLException {
+        String sql = "CALL p_registrar_emprestimo(?, ?, ?, ?)";
+
+        PreparedStatement stmt = this.conexao.prepareStatement(sql);
+        stmt.setLong(1, emprestimo.getLivro().getId());
+        stmt.setLong(2, emprestimo.getUsuario().getId());
+        if (emprestimo.getDataEmprestimo() != null) {
+            stmt.setDate(3, java.sql.Date.valueOf(emprestimo.getDataEmprestimo()));
+        } else {
+            stmt.setNull(3, Types.DATE);
+        }
+
+        if (emprestimo.getDataDevolucao() != null) {
+            stmt.setDate(4, java.sql.Date.valueOf(emprestimo.getDataDevolucao()));
+        } else {
+            stmt.setNull(4, Types.DATE);
+        }
+
+        stmt.execute();
+        stmt.close();
+    }
+
+    public void registrarDevolucao(long livroId, int ano, int mes, int dia)throws SQLException {
+        String sql = "CALL p_registrar_devolucao(?, ?)";
+
+        PreparedStatement stmt = this.conexao.prepareStatement(sql);
+        stmt.setLong(1, livroId);
+        stmt.setDate(2, java.sql.Date.valueOf(LocalDate.of(ano, mes, dia)));
+
+        stmt.execute();
+        stmt.close();
+    }
 }
